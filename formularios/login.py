4033c8d803d68f9758d5b7e7f2da453e592f6dec
guidomrauber <49770318+guidomrauber@users.noreply.github.com>
2022-10-30 20:42:40 -0300

    modificando archivo principal y login

--- formularios/login.py
+++ formularios/login.py
@@ -1,28 +1,42 @@
+import tkinter 
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-
-class Aplicacion:
-    def __init__(self):
-        self.ventana1=tk.Tk()
-        self.labelframe1=ttk.LabelFrame(self.ventana1, text="Login:")        
-        self.labelframe1.grid(column=0, row=0, padx=5, pady=10)        
-        self.login()
-
-    def login(self):
-        self.label1=ttk.Label(self.labelframe1, text="Nombre de usuario:")
-        self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.entry1=ttk.Entry(self.labelframe1)
-        self.entry1.grid(column=1, row=0, padx=4, pady=4)
-        self.label2=ttk.Label(self.labelframe1, text="Ingrese clave:")        
-        self.label2.grid(column=0, row=1, padx=4, pady=4)
-        self.entry2=ttk.Entry(self.labelframe1, show="*")
-        self.entry2.grid(column=1, row=1, padx=4, pady=4)
-        self.boton1=ttk.Button(self.labelframe1, text="Ingresar",command=self.ingresar)
-        self.boton1.grid(column=1, row=2, padx=4, pady=4)
-
-    def ingresar(self):
-        self.ventana1.destroy()
-        
+from tkinter import messagebox
 
 def mostrar():
-    aplicacion1=Aplicacion()
+    pantalla1 = tk.Tk()
+    pantalla1.geometry("800x600")
+    pantalla1.title("INICIO DE SESION")
+    pantalla1.iconbitmap("images.ico")
+    
+    
+    Label(pantalla1, text="POR FAVOR INGRESE SU USUARIO Y CONTRASEÑA",fg="navy",width="300",height="1", font=("calibri", 25)).pack()
+    Label(pantalla1,text="").pack()
+
+    global nusuario_ver
+    global contr_ver
+    nusuario_ver=StringVar()
+    contr_ver=StringVar()
+
+    global nusuario_entrar
+    global contr_entrar
+
+    Label(pantalla1, text="USUARIO").pack()
+    Label(pantalla1,text="").pack()
+    nusuario_entrar = Entry(pantalla1, textvariable=nusuario_ver)
+    nusuario_entrar.pack()
+    Label(pantalla1).pack()
+
+    Label(pantalla1, text="CONTRASEÑA").pack()
+    Label(pantalla1,text="").pack()
+    contr_entrar = Entry(pantalla1, textvariable=contr_ver)
+    contr_entrar.pack()
+    Label(pantalla1).pack()
+
+    Button(pantalla1, text = "INICIAR SESION").pack()
+
+    image = PhotoImage(file = "images.gif")
+    image = image.subsample(1,1)
+    label=Label(image=image)
+    label.pack()
--- principal.py
+++ principal.py
@@ -1,20 +1,31 @@
+import tkinter 
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter import messagebox
 import formularios.login
+def inicio_sesion():
+    formularios.login.mostrar()    
+pantalla = tk.Tk()
+pantalla.geometry("800x600")
+pantalla.title("BIENVENIDOS")
+pantalla.iconbitmap("images.ico")
+
+image = PhotoImage(file = "images.gif")
+image = image.subsample(1,1)
+label=Label(image=image)
+label.pack()
+Label(text="ACCESO AL SISTEMA",  fg="navy",width="300",height="1", font=("calibri", 25)).pack()
+
+Label(text="DEL GYM",  fg="navy",width="300",height="1", font=("calibri", 25)).pack()
+Label(text="").pack()
+Button(text="INICIAR SESION", width="30",height="3",command =inicio_sesion).pack()
+Label(text="").pack()
+Button(text="REGISTRAR", width="30",height="3").pack()
+
+pantalla.mainloop()
 
 
-class Aplicacion:
-    def __init__(self):
-        self.ventana1=tk.Tk()
-        self.ventana1.title("CONTROL DE UN GIMNASIO")        
-        
-        self.boton2=ttk.Button(self.ventana1, text="Mostrar formulario de login", command=self.mostrar_login)
-        self.boton2.grid(column=0, row=1)
-        self.ventana1.mainloop()
 
-   
-    def mostrar_login(self):
-        formularios.login.mostrar()
 
-aplicacion1=Aplicacion()
+
